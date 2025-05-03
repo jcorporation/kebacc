153b28c1a72ead5852bc4c2ebe970ad696f0d4bd
jcorporation <mail@jcgames.de>
2025-05-03 18:24:08 +0200

    Fix: Allow -p option

--- src/options.c
+++ src/options.c
@@ -64,7 +64,7 @@ static void print_usage(struct t_config *config, const char *cmd) {
 enum handle_options_rc handle_options(struct t_config *config, int argc, char **argv) {
     int n = 0;
     int option_index = 0;
-    while ((n = getopt_long(argc, argv, "l:o:vhi:w:", long_options, &option_index)) != -1) {
+    while ((n = getopt_long(argc, argv, "l:o:vhi:p:w:", long_options, &option_index)) != -1) {
         switch(n) {
             case 'l':
                 free(config->listen);
