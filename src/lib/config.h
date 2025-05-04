f3e18350714020373d9ba3a9a94ed9b257010d45
jcorporation <mail@jcgames.de>
2025-05-04 17:13:27 +0200

    Upd: docu

--- src/lib/config.h
+++ src/lib/config.h
@@ -1,27 +1,25 @@
 /*
  SPDX-License-Identifier: GPL-3.0-or-later
- (c) 2018-2025 Juergen Mang <mail@jcgames.de>
+ (c) 2025 Juergen Mang <mail@jcgames.de>
  https://github.com/jcorporation/kebacc
 */
 
 /*! \file
- * \brief Central kebacc configuration definitions
+ * \brief Central Kebacc configuration definitions
  */
 
 #ifndef KEBACC_CONFIG_H
 #define KEBACC_CONFIG_H
 
-#include "compile_time.h"
-
 /**
- * Static kebacc configuration
+ * Static Kebacc configuration
  */
 struct t_config {
-    char *rest_listen;
-    char *wallbox_listen;
-    char *wallbox_ip;
-    char *workdir;
-    unsigned long poll;
+    unsigned long poll;    //!< Poll interval for the server loop
+    char *rest_listen;     //!< Listening URI for the REST HTTP service
+    char *wallbox_listen;  //!< Listening URI for wallbox communication
+    char *wallbox_ip;      //!< Wallbox IP
+    char *workdir;         //!< Working directory
 };
 
 struct t_config *kebacc_config_new(void);
